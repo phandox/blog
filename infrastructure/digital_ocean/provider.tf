17473bcd535cf60700b8c216d7ea198479774ec7
Lukas Nagy <nagy.lukas1@gmail.com>
2018-06-05 10:56:23 +0200

    Added default values for Terraform variables

--- infrastructure/digital_ocean/provider.tf
+++ infrastructure/digital_ocean/provider.tf
@@ -1,7 +1,18 @@
 variable "do_token" {}
-variable "pub_key" {}
-variable "pvt_key" {}
-variable "ssh_fingerprint" {}
+
+variable "root_dir" {
+  default = "/home/lukas/blog"
+}
+
+variable "pub_key" {
+  default = "/home/lukas/.ssh/id_rsa_passwordless.pub"
+}
+variable "pvt_key" {
+  default = "/home/lukas/.ssh/id_rsa_passwordless"
+}
+variable "ssh_fingerprint" {
+  default = "26:ee:c4:a0:28:16:dd:51:9c:a4:bc:ae:fc:fa:d7:c9"
+}
 
 provider "digitalocean" {
   token = "${var.do_token}"
